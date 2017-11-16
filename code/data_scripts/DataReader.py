c9d3c5ebf529aa6dfa583380d2d89c32fc0cc52d
Jiachen <jiachen@Jiachens-MacBook-Pro.local>
2017-11-15 22:18:26 -0500

    solve conflict

--- code/data_scripts/DataReader.py
+++ code/data_scripts/DataReader.py
@@ -4,10 +4,7 @@ from os import listdir
 from os.path import join, isfile
 import nibabel as nib
 import numpy as np
-=======
-import numpy as np
 from utils.config import get
->>>>>>> c350650f488f696ab88ae310066d567dd0d80769
 
 def readCSVData(path):
     return pd.read_csv(path)
