89451942e9bab5faeddad8f2c1a2fd40617c63e1
Jiachen <jiachen@GEOELEMENT2.geo.lsa.umich.edu>
2017-11-20 23:24:29 -0500

    try queue

--- code/engine/vanilla_baseline.py
+++ code/engine/vanilla_baseline.py
@@ -21,10 +21,13 @@ def GetEvaluatedLoss(sess, dataSet, lossFunction, imagesPL, labelsPL):
     feed_dict = DefineFeedDict(dataSet, imagesPL, labelsPL)
     return sess.run(lossFunction, feed_dict=feed_dict)
 
-def ReportProgress(sess, step, lossFunction, imagesPL, labelsPL, splitTrainSet, splitTestSet):
+# def ReportProgress(sess, step, lossFunction, imagesPL, labelsPL, splitTrainSet, splitTestSet):
+def ReportProgress(sess, step, lossFunction, trainOperation):    
     if step % 10 == 0:
         trainFeedDict = DefineFeedDict(splitTrainSet, imagesPL, labelsPL)
         trainingLoss = GetEvaluatedLoss(sess, splitTrainSet, lossFunction, imagesPL, labelsPL)
+        # _, trainingLoss = sess.run([trainOperation, lossFunction])
+        # print("Training Loss: ", trainingLoss)
         testFeedDict = DefineFeedDict(splitTestSet, imagesPL, labelsPL)
         validationLoss = GetEvaluatedLoss(sess, splitTestSet, lossFunction, imagesPL, labelsPL)
         print('Step: %d, Evaluated Training Loss: %f, Evaluated Test Loss: %f' % (step, trainingLoss, validationLoss))
@@ -36,15 +39,19 @@ def TrainModel(sess, dataSet, imagesPL, labelsPL, predictionLayer, trainOperatio
     splitTestSet = DataSet(X_test, y_test)
 
     for batch_index in range(get('TRAIN.VANILLA_BASELINE.NB_STEPS')):
+        # batch_images, batch_labels = tf.train.shuffle_batch([X_train, X_test], batch_size = get('TRAIN.VANILLA_BASELINE.BATCH_SIZE'), capacity = get('TRAIN.VANILLA_BASELINE.CAPACITY'), min_after_dequeue = get('TRAIN.VANILLA_BASELINE.MIN_AFTER_DEQUEUE'))
         batch_images, batch_labels = splitTrainSet.next_batch(
             get('TRAIN.VANILLA_BASELINE.BATCH_SIZE'))
         feed_dict = DefineFeedDict(DataSet(batch_images, batch_labels), imagesPL, labelsPL)
-        sess.run(trainOperation, feed_dict=feed_dict)
+        # tf.train.start_queue_runners(sess = sess)
+        # sess.run(trainOperation, feed_dict=feed_dict)
         ReportProgress(sess, batch_index, lossFunction, imagesPL, labelsPL, splitTrainSet, splitTestSet)
+        # ReportProgress(sess, batch_index, lossFunction, trainOperation)
 
     trainingLoss = GetEvaluatedLoss(sess, splitTrainSet, lossFunction, imagesPL, labelsPL)
     testLoss = GetEvaluatedLoss(sess, splitTestSet, lossFunction, imagesPL, labelsPL)
     return (trainingLoss, testLoss)
+    # return trainingLoss
 
 
 def RepeatModel(dataSet, imagesPL, labelsPL, predictionLayer, trainOperation, lossFunction, numRepeats=10):
@@ -55,6 +62,7 @@ def RepeatModel(dataSet, imagesPL, labelsPL, predictionLayer, trainOperation, lo
         with tf.Session() as sess:
             init = tf.global_variables_initializer()
             sess.run(init)
+            # trainingLoss = TrainModel(sess, dataSet, imagesPL, labelsPL, predictionLayer, trainOperation, lossFunction)
             (trainingLoss, testLoss) = TrainModel(sess, dataSet, imagesPL, labelsPL, predictionLayer, trainOperation, lossFunction)
             trainingLosses.append(trainingLoss)
             testLosses.append(testLoss)
@@ -70,10 +78,10 @@ def test(dataSet):
     print('----------------------------------------------------------------')
     imagesPL, predictionLayer = fMRI_4D_CNN()
     labelsPL = tf.placeholder(tf.float32, shape=[None, 1])
-    total_error = tf.reduce_sum(tf.square(tf.sub(labelsPL, tf.reduce_mean(labelsPL))))
-    unexplained_error = tf.reduce_sum(tf.square(tf.sub(labelsPL, predictionLayer)))
-    lossFunction = tf.sub(1, tf.div(total_error, unexplained_error))
-    # lossFunction = tf.losses.mean_squared_error(labels=labelsPL, predictions=predictionLayer)
+    # total_error = tf.reduce_sum(tf.square(tf.sub(labelsPL, tf.reduce_mean(labelsPL))))
+    # unexplained_error = tf.reduce_sum(tf.square(tf.sub(labelsPL, predictionLayer)))
+    # lossFunction = tf.sub(1, tf.div(total_error, unexplained_error))
+    lossFunction = tf.losses.mean_squared_error(labels=labelsPL, predictions=predictionLayer)
     global_step = tf.Variable(0, name='global_step', trainable=False)
     trainOperation = tf.train.AdamOptimizer(get('TRAIN.VANILLA_BASELINE.LEARNING_RATE')).minimize(lossFunction, global_step=global_step)
 
