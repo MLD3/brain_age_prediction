fixed weight mul issue using numpy broadcasting

code/engine/train_baselineStructuralCNN.py
@@ -48,6 +48,15 @@ if __name__ == '__main__':
     batchSizeArray = []
     saveNames = []
 
+    with tf.variable_scope('3DConvolutionAttention'):
+        predictionLayer, lossFunction, trainOperation, stepCount, batchSize = GetCNNBaselineModel(imagesPL, trainingPL, useAttentionMap=True)
+        predictionLayers.append(predictionLayer)
+        trainOperations.append(trainOperation)
+        lossFunctions.append(lossFunction)
+        stepCountArray.append(stepCount)
+        batchSizeArray.append(batchSize)
+        saveNames.append(tf.contrib.framework.get_name_scope())
+
     with tf.variable_scope('3DConvolutionStandard'):
         predictionLayer, lossFunction, trainOperation, stepCount, batchSize = GetCNNBaselineModel(imagesPL, trainingPL)
         predictionLayers.append(predictionLayer)
@@ -95,15 +104,6 @@ if __name__ == '__main__':
 
     with tf.variable_scope('3DConvolutionExtraHiddenLayer48'):
         predictionLayer, lossFunction, trainOperation, stepCount, batchSize = GetCNNBaselineModel(imagesPL, trainingPL, optionalHiddenLayerUnits=48)
-        predictionLayers.append(predictionLayer)
-        trainOperations.append(trainOperation)
-        lossFunctions.append(lossFunction)
-        stepCountArray.append(stepCount)
-        batchSizeArray.append(batchSize)
-        saveNames.append(tf.contrib.framework.get_name_scope())
-
-    with tf.variable_scope('3DConvolutionAttention'):
-        predictionLayer, lossFunction, trainOperation, stepCount, batchSize = GetCNNBaselineModel(imagesPL, trainingPL, useAttentionMap=True)
         predictionLayers.append(predictionLayer)
         trainOperations.append(trainOperation)
         lossFunctions.append(lossFunction)

code/model/build_baselineStructuralCNN.py
@@ -40,7 +40,9 @@ def standardBlock(inputs, trainingPL, blockNumber, filters):
 
 def attentionMap(inputs):
     with tf.variable_scope('attentionMap'):
-        attentionWeight = tf.Variable(tf.ones(shape=inputs.shape, name='attentionWeight'))
+        weightShape = list(inputs.shape)
+        weightShape[0] = 1
+        attentionWeight = tf.Variable(tf.ones(shape=weightShape, name='attentionWeight'))
         return tf.multiply(inputs, attentionWeight)
 
 def baselineStructuralCNN(imagesPL, trainingPL, keepProbability=get('TRAIN.ROI_BASELINE.KEEP_PROB'), defaultActivation=tf.nn.elu, optionalHiddenLayerUnits=0, useAttentionMap=False):