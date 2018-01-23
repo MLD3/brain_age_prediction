e3dfda186f7c75b6e99978dd9dc5e1aa6a1395a2
psturm <psturm@umich.edu>
2018-01-23 15:47:13 -0500

    added a slice saver

--- code/data_scripts/DataConverter.py
+++ code/data_scripts/DataConverter.py
@@ -38,6 +38,42 @@ def ConvertNPYToBinary(inFile, outFile, SubjectDataFrame):
     accumulatedArrays = np.array(accumulatedArrays)
     accumulatedArrays.tofile('{}.bin'.format(outFile))
 
+def SpliceNIIFilesAlongAxes(inFile, SubjectDataFrame):
+    for _, row in SubjectDataFrame.iterrows():
+        subject = row['Subject']
+        print('Splicing Subject {}'.format(subject))
+        fileName = inFile + str(subject) + '.nii'
+        NIIimage = nib.load(fileName)
+        imageArray = NIIimage.get_data()
+
+        desiredDim = 145
+
+        currentWidth, currentHeight, currentDepth = imageArray.shape
+        widthPadding = desiredDim - currentWidth
+        heightPadding = desiredDim - currentHeight
+        depthPadding = desiredDim - currentDepth
+
+        xSlicesName = 'xAxisSlices/'
+        for i in range(currentWidth):
+            xSlice = imageArray[i, :, :]
+            xSlice = np.pad(xSlice, [(heightPadding, 0), (depthPadding, 0)], mode='constant')
+            assert xSlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(xSlice.Shape)
+            np.save('{}{}_x_{}'.format(xSlicesName, subject, i))
+
+        ySlicesName = 'yAxisSlices/'
+        for i in range(currentHeight):
+            ySlice = imageArray[:, i, :]
+            ySlice = np.pad(ySlice, [(widthPadding, 0), (depthPadding, 0)], mode='constant')
+            assert ySlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(ySlice.Shape)
+            np.save('{}{}_y_{}'.format(ySlicesName, subject, i))
+
+        zSlicesName = 'zAxisSlices/'
+        for i in range(currentDepth):
+            zSlice = imageArray[:, :, i]
+            zSlice = np.pad(zSlice, [(widthPadding, 0), (heightPadding, 0)], mode='constant')
+            assert zSlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(zSlice.Shape)
+            np.save('{}{}_z_{}'.format(zSlicesName, subject, i))
+
 if __name__ == '__main__':
     SubjectDataFrame = pd.read_csv('/data/psturm/PNC_724_phenotypics.csv')
     inFileStructural = '/data/psturm/structural/niftiImages/'
