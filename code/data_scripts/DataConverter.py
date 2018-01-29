9c7ec47a70d3809cbbf554be31788119113157a8
psturm <psturm@umich.edu>
2018-01-29 10:08:18 -0500

    reworking data conversion

--- code/data_scripts/DataConverter.py
+++ code/data_scripts/DataConverter.py
@@ -97,7 +97,93 @@ def SpliceNIIFilesAlongAxes(inFile, outFile, SubjectDataFrame):
             assert zSlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(zSlice.Shape)
             np.save('{}{}_z_{}'.format(zSlicesName, subject, i), zSlice)
 
+def NPYToBinaryDataset(inFile, outFile, SubjectDataFrame):
+    numRows = SubjectDataFrame.shape[0]
+    brainImages = np.zeros((numRows, 121, 145, 121))
+    exampleWidth = 121 * 145 * 121 + 1
+    ages = np.zeros((numRows))
+    index = 0
+    for _, row in SubjectDataFrame.iterrows():
+        index += 1
+        subject = row['Subject']
+        age = row['AgeYears']
+        print('Reading subject {}, {} out of {}'.format(subject, index, numRows), end='\r')
+        fileName = '{}{}.npy'.format(inFile, subject)
+        image = np.load(fileName)
+
+        brainImages[index - 1, :, :, :] = image
+        ages[index - 1] = age
+
+    indices = np.random.permutation(numRows)
+    testIndices = indices[:75]
+    valdIndices = indices[75:150]
+    trainIndices = indices[150:]
+
+    flattenedImages = brainImages.reshape((numRows, 121*145*121))
+    flattenedImages = np.insert(flattenedImages, 0, ages, axis=1)
+    print(flattenedImages.shape)
+    flattenedImages[testIndices].tofile('{}structural_test.bin'.format(outFile))
+    flattenedImages[valdIndices].tofile('{}structural_vald.bin'.format(outFile))
+    flattenedImages[trainIndices].tofile('{}structural_train.bin'.format(outFile))
+    flattenedImages = None
+
+
+    trainingImages = brainImages[trainIndices]
+    trainingAges = ages[trainIndices]
+    numTraining = numRows - 150
+    desiredDim = 145
+
+    currentWidth, currentHeight, currentDepth = imageArray.shape
+    widthPadding = desiredDim - currentWidth
+    heightPadding = desiredDim - currentHeight
+    depthPadding = desiredDim - currentDepth
+
+    flattenedImages = np.zeros((numTraining * 121, 145 * 145 + 1))
+    index = 0
+    for j in range(numTraining):
+        print('Slicing image {} of {}, x axis'.format(j, numTraining))
+        currentImage = trainingImages[j, :, :, :]
+        for i in range(121):
+            xSlice = currentImage[i, :, :]
+            xSlice = np.pad(xSlice, [(heightPadding, 0), (depthPadding, 0)], mode='constant')
+            assert xSlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(xSlice.Shape)
+            xSlice = xSlice.flatten()
+            xSlice = np.insert(xSlice, 0, age)
+            flattenedImages[index, :] = xSlice
+            index += 1
+    flattenedImages.tofile('{}xAxisSlices/train.bin'.format(outFile))
+
+    flattenedImages = np.zeros((numTraining * 145, 145 * 145 + 1))
+    index = 0
+    for j in range(numTraining):
+        print('Slicing image {} of {}, y axis'.format(j, numTraining))
+        currentImage = trainingImages[j, :, :, :]
+        for i in range(145):
+            ySlice = currentImage[:, i, :]
+            ySlice = np.pad(ySlice, [(widthPadding, 0), (depthPadding, 0)], mode='constant')
+            assert ySlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(ySlice.Shape)
+            ySlice = ySlice.flatten()
+            ySlice = np.insert(ySlice, 0, age)
+            flattenedImages[index, :] = ySlice
+            index += 1
+    flattenedImages.tofile('{}yAxisSlices/train.bin'.format(outFile))
+
+    flattenedImages = np.zeros((numTraining * 145, 145 * 145 + 1))
+    index = 0
+    for j in range(numTraining):
+        print('Slicing image {} of {}, z axis'.format(j, numTraining))
+        currentImage = trainingImages[j, :, :, :]
+        for i in range(121):
+            zSlice = currentImage[:, :, i]
+            zSlice = np.pad(zSlice, [(widthPadding, 0), (heightPadding, 0)], mode='constant')
+            assert zSlice.shape == (desiredDim, desiredDim), 'Shape {} is not correct'.format(zSlice.Shape)
+            zSlice = zSlice.flatten()
+            zSlice = np.insert(zSlice, 0, age)
+            flattenedImages[index, :] = zSlice
+            index += 1
+    flattenedImages.tofile('{}zAxisSlices/train.bin'.format(outFile))
+
+
 if __name__ == '__main__':
     SubjectDataFrame = pd.read_csv('/data/psturm/PNC_724_phenotypics.csv')
-    ConvertNPYToBinary(inFile='/data/psturm/structural/yAxisSlices/', outFile='/data/psturm/structural/yAxisSlices/dataSet', SubjectDataFrame=SubjectDataFrame, maxDims=145, midChar='y')
-    ConvertNPYToBinary(inFile='/data/psturm/structural/zAxisSlices/', outFile='/data/psturm/structural/zAxisSlices/dataSet', SubjectDataFrame=SubjectDataFrame, maxDims=121, midChar='z')
+    NPYToBinaryDataset(inFile='/data/psturm/structural/numpyArrays', outFile='/data/psturm/structural', SubjectDataFrame=SubjectDataFrame)
