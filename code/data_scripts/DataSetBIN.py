3886d5506109407d0aceb61ddad3608a181c6122
psturm <psturm@umich.edu>
2018-01-29 15:05:19 -0500

    fixed a reshaping issue

--- code/data_scripts/DataSetBIN.py
+++ code/data_scripts/DataSetBIN.py
@@ -70,6 +70,7 @@ class DataSetBIN(object):
                                  [[0,0], [24, 0], [0,0], [0,0]])
                 images = tf.concat([imagesX, imagesY, imagesZ], axis=0)
             self.imageBatchOperation = images
+            self.labelBatchOperation = tf.reshape(self.selfLabelBatchOperation, shape=())
 
     def GetBatchOperations(self):
         return self.imageBatchOperation, self.labelBatchOperation
