fixed bug: now doing MAX rather than AVERAGE pooling, also general updates

code/engine/trainCommon.py
@@ -8,6 +8,7 @@ from utils import saveModel
 from utils.config import get
 from placeholders.shared_placeholders import *
 from datetime import datetime
+from utils.args import *
 
 class PrintOps(object):
     def __init__(self, ops, updateOps, names):
@@ -74,17 +75,21 @@ class ModelTrainer(object):
 
     def GetPerformanceThroughSet(self, sess, printOps, setType='vald', batchTrainFeedDict=None):
         sess.run(tf.local_variables_initializer())
+        accumulatedOps = sess.run(printOps.ops)
         if setType == 'vald':
-            feed_dict = feed_dict=self.GetFeedDict(sess, setType=setType)
             numberIters = self.valdSet.maxItemsInQueue
         elif setType == 'test':
-            feed_dict = feed_dict=self.GetFeedDict(sess, setType=setType)
             numberIters = self.testSet.maxItemsInQueue
         elif setType == 'train':
-            feed_dict = batchTrainFeedDict
             numberIters = 1
-
+        
         for i in range(numberIters):
+            if setType == 'vald':
+                feed_dict = self.GetFeedDict(sess, setType=setType)
+            elif setType == 'test':
+                feed_dict = self.GetFeedDict(sess, setType=setType)
+            elif setType == 'train':
+                feed_dict = batchTrainFeedDict
             sess.run(printOps.updateOps, feed_dict=feed_dict)
 
         accumulatedOps = sess.run(printOps.ops)
@@ -190,17 +195,17 @@ class ModelTrainer(object):
         outputFile = open('{}performance.txt'.format(self.summaryDir), 'w')
 
         print("==============Validation Set Operations, Best==============")
-        outputFile.write("==============Validation Set Operations, Best==============")
+        outputFile.write("==============Validation Set Operations, Best==============\n")
         for opName in bestValdOpDict:
             outputString = '{}: {} +- {}'.format(opName, np.mean(bestValdOpDict[opName]), np.std(bestValdOpDict[opName]))
             print(outputString)
-            outputFile.write(outputString)
+            outputFile.write(outputString + '\n')
         print("==============Test Set Operations, Best==============")
-        outputFile.write("==============Test Set Operations, Best==============")
+        outputFile.write("==============Test Set Operations, Best==============\n")
         for opName in bestTestOpDict:
             outputString = '{}: {} +- {}'.format(opName, np.mean(bestTestOpDict[opName]), np.std(bestTestOpDict[opName]))
             print(outputString)
-            outputFile.write(outputString)
+            outputFile.write(outputString + '\n')
         outputFile.close()
         coord.request_stop()
         coord.join(threads)

code/engine/trainCustomCNN.py
@@ -50,8 +50,8 @@ def DefineDataOpts(data='PNC', summaryName='test_comp'):
         GlobalOpts.labelBaseString = get('DATA.LABELS')
         GlobalOpts.imageBatchDims = (-1, 41, 49, 41, 1)
         GlobalOpts.trainBatchSize = 4
-        GlobalOpts.numberTestItems = 75
-        GlobalOpts.numberValdItems = 75
+        GlobalOpts.numberTestItems = 100
+        GlobalOpts.numberValdItems = 100
     elif data == 'ABIDE1':
         GlobalOpts.trainFiles = np.load(get('ABIDE.ABIDE1.TRAIN_LIST')).tolist()
         GlobalOpts.valdFiles = np.load(get('ABIDE.ABIDE1.VALD_LIST')).tolist()
@@ -72,8 +72,12 @@ def DefineDataOpts(data='PNC', summaryName='test_comp'):
         GlobalOpts.trainBatchSize = 4
         GlobalOpts.numberTestItems = get('ABIDE.ABIDE2.NUM_TEST')
         GlobalOpts.numberValdItems = get('ABIDE.ABIDE2.NUM_VALD')
-    GlobalOpts.poolType = 'AVERAGE'
+    GlobalOpts.poolType = 'MAX'
     GlobalOpts.name = '{}Scale{}Data{}'.format(GlobalOpts.type, GlobalOpts.scale, data)
+    if GlobalOpts.sliceIndex is not None:
+        GlobalOpts.name = '{}Slice{}'.format(GlobalOpts.name, GlobalOpts.sliceIndex)
+    if GlobalOpts.align:
+        GlobalOpts.name = '{}Aligned'.format(GlobalOpts.name)
     GlobalOpts.summaryDir = '../summaries/{}/{}/'.format(summaryName,
                                                      GlobalOpts.name)
     GlobalOpts.checkpointDir = '../checkpoints/{}/{}/'.format(summaryName,
@@ -120,16 +124,42 @@ def compareCustomCNN():
         'required': True
         },
         {
+        'flag': '--summaryName',
+        'help': 'The file name to put the results of this run into.',
+        'action': 'store',
+        'type': str,
+        'dest': 'summaryName',
+        'required': True
+        },
+        {
         'flag': '--data',
         'help': 'One of: PNC, ABIDE1, ABIDE2',
         'action': 'store',
         'type': str,
         'dest': 'data',
         'required': True
+        },
+        {
+        'flag': '--sliceIndex',
+        'help': 'Set this to an integer to select a single brain region as opposed to concatenating all regions along the depth channel.',
+        'action': 'store',
+        'type': int,
+        'dest': 'sliceIndex',
+        'required': False,
+        'const': None
+        },
+        {
+        'flag': '--align',
+        'help': 'Set to true to align channels.',
+        'action': 'store',
+        'type': int,
+        'dest': 'align',
+        'required': False,
+        'const': None
         }
         ]
     ParseArgs('Run 3D CNN over structural MRI volumes', additionalArgs=additionalArgs)
-    DefineDataOpts(data=GlobalOpts.data)
+    DefineDataOpts(data=GlobalOpts.data, summaryName=GlobalOpts.summaryName)
 
     modelTrainer = ModelTrainer()
     trainDataSet, valdDataSet, testDataSet = GetDataSetInputs()
@@ -150,7 +180,9 @@ def compareCustomCNN():
                             GlobalOpts.scale,
                             convLayers,
                             fullyConnectedLayers,
-                            poolType=GlobalOpts.poolType)
+                            poolType=GlobalOpts.poolType,
+                            sliceIndex=GlobalOpts.sliceIndex,
+                            align=GlobalOpts.align)
     lossOp, printOps = GetOps(labelsPL, outputLayer)
     learningRate = 0.0001
     updateOp = GetTrainingOperation(lossOp, learningRate)

code/model/buildCustomCNN.py
@@ -13,13 +13,15 @@ def customCNN(imagesPL,
               keepProbability=0.6,
               convStrides=None,
               poolStrides=None,
-              poolType='MAX'):
+              poolType='MAX',
+              sliceIndex=None,
+              align=False):
     with tf.variable_scope('customCNN'):
         if imagesPL.dtype != tf.float32:
             imagesPL = tf.cast(imagesPL, tf.float32, name='CastInputToFloat32')
         if scale is not None:
             with tf.variable_scope('PatchExtraction'):
-                imagesPL = ExtractImagePatches3D(imagesPL, scale=scale)
+                imagesPL = ExtractImagePatches3D(imagesPL, scale=scale, sliceIndex=sliceIndex, align=align)
         index = 0
         for numFilters in convolutionalLayers:
             convStride = (1,1,1)
@@ -36,6 +38,6 @@ def customCNN(imagesPL,
             for numUnits in fullyConnectedLayers[:-1]:
                 hiddenLayer = standardDense(hiddenLayer, units=numUnits, name='hiddenLayer{}'.format(numUnits))
                 hiddenLayer = tf.contrib.layers.dropout(inputs=hiddenLayer, keep_prob=keepProbability, is_training=trainingPL)
-        outputUnits = fullyConnectedLayers[-1]
-        outputLayer = standardDense(hiddenLayer, units=outputUnits, activation=None, use_bias=False)
+            outputUnits = fullyConnectedLayers[-1]
+            outputLayer = standardDense(hiddenLayer, units=outputUnits, activation=None, use_bias=False, name='outputLayer')
         return outputLayer

code/utils/patches.py
@@ -34,7 +34,7 @@ def ExtractImagePatchesDEPRECATED(images, strideSize, kernelSize=3):
     imagePatches = tf.concat(patches, axis=4)
     return imagePatches
 
-def ExtractImagePatches3D(images, scale=2, kernelSize=3):
+def ExtractImagePatches3D(images, scale=2, kernelSize=3, sliceIndex=None, align=False):
     if scale == 1:
         return images
     _, numRows, numCols, depth, _ = images.get_shape().as_list()
@@ -42,6 +42,11 @@ def ExtractImagePatches3D(images, scale=2, kernelSize=3):
     rowStride = int((numRows - 2 * kernelSize) / scale)
     colStride = int((numCols - 2 * kernelSize) / scale)
     depthStride = int((depth - 2 * kernelSize) / scale)
+    runningIndex = 0
+    X = 1
+    Y = 2
+    Z = 3
+    alignAxes=[None, [Z], [Y], [Y, Z], [X], [X, Z], [X, Y], [X, Y, Z]]
     
     rowIndex = rowStride + kernelSize
     while rowIndex <= numRows:
@@ -56,6 +61,12 @@ def ExtractImagePatches3D(images, scale=2, kernelSize=3):
                                     max(colIndex-colStride-kernelSize, 0):min(colIndex+kernelSize, numCols),
                                     max(depthIndex-depthStride-kernelSize, 0):min(depthIndex+kernelSize, depth),
                                     :]
+                if sliceIndex is not None and sliceIndex == runningIndex:
+                    return imageSlice
+                if align and alignAxes[runningIndex] is not None:
+                    imageSlice = tf.reverse(imageSlice, axis=alignAxes[runningIndex])
+                
+                runningIndex += 1
                 patches.append(imageSlice)
                 depthIndex += depthStride
             colIndex += colStride